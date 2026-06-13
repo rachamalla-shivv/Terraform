6faa2c2d58f8889f7435d6902e8265d887825a94
rachamalla-shivv <rachamallashivashankargoud0@gmail.com>
2026-06-13 22:40:41 +0530

    env

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -6,6 +6,38 @@ resource "aws_instance" "terraform" {
         Name = "terraform-1"
         Terraform = "true"
     }
+
+    provisioner "local-exec"{
+      command = "echo ${self.private_ip} > inventory"
+      on_failure = continue
+    }
+
+    provisioner "local-exec"{
+      command = "echo Instance is destroyed"
+      when    = destroy
+    }
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+      ]
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo systemctl stop nginx",
+        "echo 'successfully stopped nginx server' "
+      ]
+      when = destroy
+    }
 }
 
 resource "aws_security_group" "allow_all" {
